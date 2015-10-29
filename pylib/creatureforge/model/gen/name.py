f2270393e3232d5da8a010316c392038211d6491
Eddie <eddiepie@gmail.com>
2015-10-29 20:08:00 +1300

    Refactored name model

--- pylib/creatureforge/model/gen/name.py
+++ pylib/creatureforge/model/gen/name.py
@@ -10,24 +10,24 @@ import re
 import logging
 from copy import deepcopy
 
-from creatureforge.exceptions import InvalidNameError
+# from creatureforge.exceptions import InvalidNameError
 
-from maya import cmds
+# from maya import cmds
 
 logging.basicConfig()
 logger = logging.getLogger(__name__)
 
-# ------------------------------------------------------------------------------
-
 MAX = 6
 DEFAULT_SUFFIX = "grp"
 
-PATTERN_POSITION = "^([a-zA-Z]+)$"
-PATTERN_DESCRIPTION = "^(?!^\d+)(?!^\d+$)([a-zA-Z0-9]+)$"
-PATTERN_INDEX = "^(\d+)$"
-PATTERN_SUFFIX = "^([a-zA-Z]+)[\d+]?$"
+PATTERN_POSITION = r"^([a-zA-Z]+)$"
+PATTERN_DESCRIPTION = r"^(?!^\d+)(?!^\d+$)([a-zA-Z0-9]+)$"
+PATTERN_INDEX = r"^(\d+)$"
+PATTERN_SUFFIX = r"^([a-zA-Z]+)[\d+]?$"
 
-# ------------------------------------------------------------------------------
+
+class InvalidNameError(Exception):
+    pass
 
 
 def validate(func):
@@ -39,27 +39,91 @@ def validate(func):
     return wraps
 
 
+def match(pattern, key, value):
+    """Match a pattern with a given value.
+
+    Args:
+        pattern (str): Regex pattern
+        key (str): Description of matched token
+        value (str): Value to match against regex pattern
+
+    Raises:
+        NameError: If value does not match regex pattern
+
+    Returns:
+        value (str): Matched regex group
+    """
+
+    try:
+        return re.match(str(pattern), str(value)).group(0)
+    except (AttributeError, TypeError):
+        err = "Invalid token '{0}': {1}".format(key, value)
+        raise NameError(err)
+
+
 class NameModel(object):
 
     SEP = "_"
 
+    POSITION = "position"
+    PRIMARY = "primary"
+    PRIMARY_INDEX = "primary_index"
+    SECONDARY = "secondary"
+    SECONDARY_INDEX = "secondary_index"
+    SUFFIX = "suffix"
+
+    @staticmethod
+    def fmt_position(position):
+        args = (PATTERN_DESCRIPTION, NameModel.POSITION, position)
+        return match(*args).upper()
+
+    @staticmethod
+    def fmt_primary(primary):
+        args = (PATTERN_DESCRIPTION, NameModel.PRIMARY, primary)
+        return match(*args)
+
+    @staticmethod
+    def fmt_primary_index(primary_index):
+        args = (PATTERN_INDEX, NameModel.PRIMARY_INDEX, str(primary_index))
+        return int(match(*args))
+
+    @staticmethod
+    def fmt_secondary(secondary):
+        args = (PATTERN_DESCRIPTION, NameModel.SECONDARY, secondary)
+        return match(*args)
+
+    @staticmethod
+    def fmt_secondary_index(secondary_index):
+        args = (PATTERN_INDEX, NameModel.SECONDARY_INDEX, str(secondary_index))
+        return int(match(*args))
+
+    @staticmethod
+    def fmt_suffix(suffix):
+        args = (PATTERN_SUFFIX, NameModel.SUFFIX, suffix)
+        return match(*args)
+
     def __init__(self, position, primary, primary_index, secondary,
                  secondary_index, suffix):
 
-        self.__set_position(position)
-        self.__set_primary(primary)
-        self.__set_primary_index(primary_index)
-        self.__set_secondary(secondary)
-        self.__set_secondary_index(secondary_index)
-        self.__set_suffix(suffix)
+        self.__position = NameModel.fmt_position(position)
+        self.__primary = NameModel.fmt_primary(primary)
+        self.__primary_index = NameModel.fmt_primary_index(primary_index)
+        self.__secondary = NameModel.fmt_secondary(secondary)
+        self.__secondary_index = NameModel.fmt_secondary_index(secondary_index)
+        self.__suffix = NameModel.fmt_suffix(suffix)
+
+        # self.__data = {}
 
     def __str__(self):
-        return self.compile()
+        return self.__compile()
 
     def __repr__(self):
             return "<{cls} '{name}'>".format(
                 cls=self.__class__.__name__,
                 name=self.compile())
+
+    def __compile(self):
+        return "{sep}".format(sep=NameModel.SEP).join(map(str, self.tokens))
 
     @property
     def position(self):
@@ -71,7 +135,7 @@ class NameModel(object):
 
     @property
     def primary_index(self):
-        return self.__primary_index
+        return int(self.__primary_index)
 
     @property
     def secondary(self):
@@ -79,88 +143,30 @@ class NameModel(object):
 
     @property
     def secondary_index(self):
-        return self.__secondary_index
+        return int(self.__secondary_index)
 
     @property
     def suffix(self):
         return self.__suffix
 
-    def tokenize(self):
-        return self.get_components()[:-1]
-
-    def get_components(self):
-        return [self.position,
+    @property
+    def tokens(self):
+        return (self.position,
                 self.primary,
-                str(self.primary_index),
+                self.primary_index,
                 self.secondary,
-                str(self.secondary_index),
-                self.suffix]
+                self.secondary_index,
+                self.suffix)
 
-    def get_data(self):
-        return {"position": self.position,
-                "primary": self.primary,
-                "primary_index": self.primary_index,
-                "secondary": self.secondary,
-                "secondary_index": self.secondary_index,
-                "suffix": self.suffix}
-
-    def compile(self):
-        components = [self.position,
-                      self.primary,
-                      str(self.primary_index),
-                      self.secondary,
-                      str(self.secondary_index),
-                      self.suffix]
-        return ("{sep}".format(sep=NameModel.SEP)).join(components)
-
-    def __copy(self):
-        return deepcopy(self)
-
-    def generate_primary(self):
-        return self.__generate("primary_index")
-
-    def generate_secondary(self):
-        return self.__generate("secondary_index")
-
-    def __generate(self, key):
-        new = self.__copy()
-        while cmds.objExists(new.compile()):
-            index = getattr(new, key)
-            data = new.get_data()
-            data.update({key: index + 1})
-            new = NameModel(**data)
-        return new.compile()
-
-    def __match(self, pattern, value, err):
-        try:
-            return re.match(pattern, value).group(0)
-        except (AttributeError, TypeError):
-            raise NameError(err)
-
-    def __set_position(self, val):
-        err = "Invalid position: '{val}'".format(val=val)
-        self.__position = str(self.__match(PATTERN_POSITION, val, err)).upper()
-
-    def __set_primary(self, val):
-        err = "Invalid primary: '{val}'".format(val=val)
-        self.__primary = self.__match(PATTERN_DESCRIPTION, val, err)
-
-    def __set_primary_index(self, val):
-        err = "Invalid primary: '{val}'".format(val=val)
-        self.__primary_index = int(self.__match(PATTERN_INDEX, str(val), err))
-
-    def __set_secondary(self, val):
-        err = "Invalid secondary: '{val}'".format(val=val)
-        self.__secondary = self.__match(PATTERN_DESCRIPTION, val, err)
-
-    def __set_secondary_index(self, val):
-        err = "Invalid secondary: '{val}'".format(val=val)
-        self.__secondary_index = int(self.__match(PATTERN_INDEX, str(val), err))
-
-    def __set_suffix(self, val):
-        err = "Invalid suffix: '{val}'".format(val=val)
-        self.__suffix = self.__match(PATTERN_SUFFIX, val, err)
-
-    def __tokenize(self):
-        return (self.position, self.primary, self.primary_index,
-                self.secondary, self.secondary_index)
+    # @property
+    # def data(self):
+    #     if not self.__data:
+    #         self.__data = {
+    #             NameModel.POSITION: self.position,
+    #             NameModel.PRIMARY: self.primary,
+    #             NameModel.PRIMARY_INDEX: self.primary_index,
+    #             NameModel.SECONDARY: self.secondary,
+    #             NameModel.SECONDARY_INDEX: self.secondary_index,
+    #             NameModel.SUFFIX: self.suffix,
+    #         }
+    #     return self.__data
